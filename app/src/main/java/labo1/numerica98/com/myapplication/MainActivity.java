cd43dd0da9d1b1002118068b0f66d5b74fceed7a
luist23 <00127717@uca.edu.sv>
2019-03-08 10:16:49 -0600

    initial commit

---
+++ app/src/main/java/labo1/numerica98/com/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package labo1.numerica98.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
